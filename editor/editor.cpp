47742bde43538b02a83103206eec6db201918b11
Davide Tuccilli <d.tuccilli@gmail.com>
2017-01-15 13:40:58 +0100

    Fixed render module change not keeping the configuration

--- editor/editor.cpp
+++ editor/editor.cpp
@@ -396,15 +396,17 @@ void editor::create_menu(void)
 				instance->stop_rendering();
 				instance->pause();
 
-				if (oldModule)
-				{
-					oldModule->shutdown();
-					xng_delete oldModule;
-				}
-
 				if (newModule && newModule->init(instance->get_window()))
 				{
 					instance->set_render_module(newModule);
+
+					if (oldModule)
+					{
+						newModule->configuration() = oldModule->configuration();
+
+						oldModule->shutdown();
+						xng_delete oldModule;
+					}
 				}
 				
 				instance->unpause();
